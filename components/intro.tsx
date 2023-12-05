193a5a424c72f366a083c847aeeac580d02922a8
Yun Chang <yunjae0500@yahoo.com>
2023-12-05 01:07:19 -0700

    Add bio in intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,23 +1,56 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
-        <div>
-          <Image
-            src="/profile-pic.png"
-            alt="Yun's portrait"
-            width="192"
-            height="192"
-            quality="95"
-            priority={true}
-            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-          />
-          <span>🏐</span>
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src="/profile-pic.png"
+              alt="Yun's portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute -bottom-2 right-1 text-3xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            🏐{" "}
+          </motion.span>
         </div>
       </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium  !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Yun.</span> I'm a {""}
+        <span className="font-bold">front-end developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
+        building
+        <span className="italic"> sites & apps</span>. My focus is{""}
+        <span className="underline font-bold"> React (Next.js)</span>.
+      </motion.p>
     </section>
   );
 }
