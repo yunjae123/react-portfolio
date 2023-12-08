168aabd7baf9245d8420dc2dbdb2625b33a8f445
Yun Chang <yunjae0500@yahoo.com>
2023-12-08 01:29:10 -0700

    Make contact me/downloadCV/linkedin/github icons. Download CV is functional

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -51,6 +55,36 @@ export default function Intro() {
         <span className="italic"> sites & apps</span>. My focus is{""}
         <span className="underline font-bold"> React (Next.js)</span>.
       </motion.p>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.2,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1" />
+        </a>
+        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+          <BsLinkedin />
+        </a>
+        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
