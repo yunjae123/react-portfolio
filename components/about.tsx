d597be7d37cd4536679357da4c9df2d0dfe88bef
Yun Chang <yunjae0500@yahoo.com>
2023-12-10 01:36:57 -0700

    Add sectionHeading to make about me text dynamic and reusable

---
+++ components/about.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+
+export default function About() {
+  return (
+    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        While in the process of graduating with a degree in{" "}
+        <span className="font-medium">Computer Science</span>, I decided to
+        pursue my passion for programming. I enrolled in a coding bootcamp and
+        learned <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">React, Python, Node.js, and Next.js</span>
+        . I am also familiar with TypeScript. I am always looking to learn new
+        technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a front-end
+        software developer.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        volleyball, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">
+          how to create and edit entertaining media
+        </span>
+        . I'm also learning how to play the guitar.
+      </p>
+    </section>
+  );
+}
