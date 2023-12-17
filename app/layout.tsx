ab2568db1968aa359d637f11d00099157b368b8a
Yun Jae <yunjae0500@yahoo.com>
2023-12-16 18:24:55 -0700

    Add timeline and contact page features

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         {/* sm:w-[68.75rem] - This means that when the screen size is LARGER than 640px(sm), the div's width will turn into 68.75rem (1100px)*/}
