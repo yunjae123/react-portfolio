edfb01194e812e3c061ef07a109ef885a5befb58
Yun Chang <yunjae0500@yahoo.com>
2023-12-25 17:50:27 -0700

    Make contact form fully cuntional with toast alerts and error checking

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -2,6 +2,9 @@
 
 import { Resend } from "resend";
 import { validateString } from "@/lib/utils";
+import { getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -17,19 +20,24 @@ export const sendEmail = async (formData: FormData) => {
     error: "Invalid message";
   }
 
+  let data;
   try {
-    await resend.emails.send({
+    data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
       to: "yunjae0500@yahoo.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error: unknown) {
-    if (error instanceof Error) {
-      return {
-        error: error.message,
-      };
-    }
+    return {
+      error: getErrorMessage(error),
+    };
   }
+  return {
+    data,
+  };
 };
