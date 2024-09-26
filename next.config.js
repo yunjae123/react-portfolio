11c64e204b4311ac288e40a6515f18f718e3b94f
Yun Jae Chang <yunjae0500@yahoo.com>
2024-09-26 04:06:06 -0600

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
-        ignoreBuildErrors: true
-      },
-      eslint: {
-        ignoreDuringBuilds: true
-      }
-};
-
-module.exports = nextConfig;
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+      config.resolve.fallback = {
+        fs: false,   // Prevents `fs` module resolution issues (used for file system)
+        path: false, // Prevents `path` resolution issues
+        os: false,   // Prevents `os` resolution issues
+      };
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
