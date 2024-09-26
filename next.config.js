5e2db567493482bef6f888296d2dcfcb114b4784
Yun Jae Chang <yunjae0500@yahoo.com>
2024-09-26 04:11:33 -0600

    change next version

--- next.config.js
+++ next.config.js
@@ -5,15 +5,7 @@ const nextConfig = {
     },
     eslint: {
       ignoreDuringBuilds: true,
-    },
-    webpack: (config) => {
-      config.resolve.fallback = {
-        fs: false,   // Prevents `fs` module resolution issues (used for file system)
-        path: false, // Prevents `path` resolution issues
-        os: false,   // Prevents `os` resolution issues
-      };
-      return config;
-    },
+    }
   };
   
   module.exports = nextConfig;
